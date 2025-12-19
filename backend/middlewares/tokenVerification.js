94e308b1204eb0e3c76874b98e2674b020579c9c
kaushalayu <kaushalayush95@gmail.com>
2025-12-19 14:55:10 +0530

    modified

--- backend/middlewares/tokenVerification.js
+++ backend/middlewares/tokenVerification.js
@@ -1,6 +1,6 @@
 const createHttpError = require("http-errors");
 const jwt = require("jsonwebtoken");
-const config = require("../config/config");
+const config = require('../Config/config');
 const User = require("../models/userModel");
 
 const isVerifiedUser = async (req, res, next) => {
